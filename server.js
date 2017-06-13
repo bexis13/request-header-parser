21af4d4b11081cabe887a3a6c74a260d533e35b5
Abose U <bexis18@gmail.com>
2017-06-13 10:05:14 +0000

    change arrangement of port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-var port = 8080 || process.env.PORT;
+var port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
