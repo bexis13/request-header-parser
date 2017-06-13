1c667ccd0a41fe72bf9ca354d848803f776d075b
Abose U <bexis18@gmail.com>
2017-06-13 07:57:11 +0000

    initial commit, add all project files and develop api

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+var port = 8080 || process.env.PORT;
+
+app.get("/api/whoami", function(request, response){
+    var ipAddress = request.headers["x-forwarded-for"];
+    
+    var language = request.headers["accept-language"];
+    language = language.split(",");
+    language = language[0];
+    
+    var software = request.headers["user-agent"];
+    software = software.split(/[()]+/);
+    software = software[1];
+    
+    response.send({ipAddress: ipAddress, language: language, software: software});
+    
+})
+
+app.listen(port, function(){
+    console.log("app is listening on port");
+})
